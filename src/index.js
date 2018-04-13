bffad3c7c8a60d6dd6fe864538ef76757d051b67
Forrest Wilson <forrest.wilson@me.com>
2018-04-13 22:32:55 +1200

    End of lesson 13

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const API_KEY = "AIzaSyDbfIaizEXduqymPy1PIWBsWhIsdA1KWPk";
+
 // Create a new component. This component should produce some HTML
 
 const App = () => {
