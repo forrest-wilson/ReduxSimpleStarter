2e0fd6b2ddf349930daf150c5a8f11dd1f91e89d
Forrest Wilson <forrest.wilson@me.com>
2018-04-15 15:17:05 +1200

    End of lesson 22

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
-
 import SearchBar from "./components/search_bar";
-
 const API_KEY = "AIzaSyDbfIaizEXduqymPy1PIWBsWhIsdA1KWPk";
 
-YTSearch({key: API_KEY, term: "surfboards"}, function(data) {
-    console.log(data);
-});
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-// Create a new component. This component should produce some HTML
+        this.state = { videos: [] };
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+        YTSearch({key: API_KEY, term: "surfboards"}, videos => {
+            // ES6 syntax for setting an object with identical key/value named pairs
+            // Will resolve to this.setState({ videos: videos });
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 };
-
-// Take this component's generated HTML and put it into the DOM
 
 ReactDOM.render(<App />, document.querySelector(".container"));
