5d0316985faf14aa3dfc43c19bd394ff29401d7f
Forrest Wilson <forrest.wilson@me.com>
2018-04-15 15:08:18 +1200

    End of lesson 21

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
 
 import SearchBar from "./components/search_bar";
 
 const API_KEY = "AIzaSyDbfIaizEXduqymPy1PIWBsWhIsdA1KWPk";
+
+YTSearch({key: API_KEY, term: "surfboards"}, function(data) {
+    console.log(data);
+});
 
 // Create a new component. This component should produce some HTML
 
