91f1ca93e2fa324c34ef976e97a30738d670aa27
Forrest Wilson <forrest.wilson@me.com>
2018-04-15 16:15:35 +1200

    End of lesson 29

--- src/index.js
+++ src/index.js
@@ -10,12 +10,18 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { videos: [] };
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
 
         YTSearch({key: API_KEY, term: "surfboards"}, videos => {
             // ES6 syntax for setting an object with identical key/value named pairs
-            // Will resolve to this.setState({ videos: videos });
-            this.setState({ videos });
+            // Will resolve this.setState({ videos }) to this.setState({ videos: videos });
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
         });
     }
 
@@ -23,8 +29,11 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
-                <VideoDetail video={this.state.videos[0]} />
-                <VideoList videos={this.state.videos}/>
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos}
+                />
             </div>
         );
     }
