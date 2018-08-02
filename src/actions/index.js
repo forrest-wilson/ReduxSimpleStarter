5b230d04a8512fa6c459b9d2bd8f154ea2e54fa1
Forrest Wilson <forrest.wilson@me.com>
2018-08-02 14:39:06 +1200

    Finish up

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const API_KEY = "a6d067f30c4840896640976aab5b8399";
+const API_KEY = 'a6d067f30c4840896640976aab5b8399';
 const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
-export const FETCH_WEATHER = "FETCH_WEATHER"
+export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`;
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,13 +1,13 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
-import { fetchWeather } from "../actions/index";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
 class SearchBar extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { term: "" };
+        this.state = { term: '' };
 
         this.onInputChange = this.onInputChange.bind(this);
         this.onFormSubmit = this.onFormSubmit.bind(this);
